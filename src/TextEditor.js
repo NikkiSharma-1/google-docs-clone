9d58d355ea89f1026bbc70c7e749b494c785edc4
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-05-19 22:47:41 +0530

    commit

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,22 +1,35 @@
 import { useCallback } from "react"
-import React from 'react';
 import Quill from "quill"
-import "quill/dist/quill.snow.css";
+import "quill/dist/quill.snow.css"
 
-function TextEditor() {
-    const wrapperRef = useCallback(wrapper => {
-        if (wrapper == null) return
-    
-        wrapper.innerHTML = ""
-        const editor = document.createElement("div")
-        wrapper.append(editor)
-        new Quill(editor, {
-          theme: "snow",
-        })
 
-    });
+const SAVE_INTERVAL_MS = 2000
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
 
-    return <div className="container" ref={wrapperRef}></div>
+export default function TextEditor() {
+  
+  const wrapperRef = useCallback(wrapper => {
+    if (wrapper == null) return
+
+    wrapper.innerHTML = ""
+    const editor = document.createElement("div")
+    wrapper.append(editor)
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    })
+    q.disable()
+    q.setText("Loading...")
+  }, [])
+  return <div className="container" ref={wrapperRef}></div>
 }
-
-export default TextEditor
